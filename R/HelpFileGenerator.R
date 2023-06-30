37f90a671e40c8c41f83494687a9d4680ee78c9b
jaxlub <98245711+jaxlub@users.noreply.github.com>
2023-06-30 13:51:13 -0400

    All set other then help file generation

---
+++ R/HelpFileGenerator.R
@@ -0,0 +1,17 @@
+#' UVMProteomics
+#'
+#' This package is designed for the UVM Proteomics Core to finds the
+#' 7 residues before and after given accessions and phosphosites of interest.
+#'
+#' @docType package
+#'
+#' @author Emily Guswa \email{Emily.Guswa@uvm.edu}, James (Jax) Lubkowitz \email{jalubk20@stlawu.edu}
+#'
+#' @import dplyr
+#' @import readxl
+#' @import tidyr
+#' @import stringr
+#'
+#' @name UVMProteomics
+NULL
+
